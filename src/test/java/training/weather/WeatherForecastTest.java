17bef1019129f57f0431463538c44d119ae08cd7
DESKTOP-6U50I7I\danie <zeibel17@gmail.com>
2022-11-14 21:38:17 +0100

    Definicion de pruebas

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -1,17 +1,33 @@
 package training.weather;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Date;
+
 import org.junit.Test;
 
 public class WeatherForecastTest {
 
 	@Test
-	public void unfinished_test() throws IOException {
-		WeatherForecast weatherForecast = new WeatherForecast();
-		String forecast = weatherForecast.getCityWeather("Madrid", new Date());
-		System.out.println(forecast);
+	public void testWeatherForecastMadrid() throws IOException {
+		String forecast = WeatherForecast.getCityWeather("Madrid", new Date());
+		assertNotNull("Predicción no obtenida", forecast);
 	}
+
+	@Test
+	public void testWeatherForecastEmptyCity() throws IOException {
+		String forecast = WeatherForecast.getCityWeather("", new Date());
+		assertEquals("", forecast);
+	}
+
+	@Test
+	public void testWeatherForecastFutureDate() throws IOException {
+		String forecast = WeatherForecast.getCityWeather("Madrid", new Date(new Date().getTime() + (618400000)));
+	assertNotNull("Predicción no obtenida", forecast);
+		assertEquals("", forecast);
+	}
+
 }
