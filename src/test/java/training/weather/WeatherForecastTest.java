8db84750304aee95edaa1859be15e5655ef38336
DESKTOP-6U50I7I\danie <zeibel17@gmail.com>
2022-11-14 23:06:54 +0100

    Modificación de test

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -2,7 +2,6 @@ package training.weather;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 import java.util.Date;
@@ -11,23 +10,39 @@ import org.junit.Test;
 
 public class WeatherForecastTest {
 
+	// Prueba con una ciudad real y la fecha actual
 	@Test
 	public void testWeatherForecastMadrid() throws IOException {
 		String forecast = WeatherForecast.getCityWeather("Madrid", new Date());
 		assertNotNull("Predicción no obtenida", forecast);
 	}
 
+	// Prueba con la ciudad en blanco y la fecha actual
 	@Test
 	public void testWeatherForecastEmptyCity() throws IOException {
 		String forecast = WeatherForecast.getCityWeather("", new Date());
 		assertEquals("", forecast);
 	}
 
+	// Prueba con una ciudad en real y la fecha futura a más de 6 días
 	@Test
 	public void testWeatherForecastFutureDate() throws IOException {
 		String forecast = WeatherForecast.getCityWeather("Madrid", new Date(new Date().getTime() + (618400000)));
-	assertNotNull("Predicción no obtenida", forecast);
+		assertNotNull("Predicción no obtenida", forecast);
 		assertEquals("", forecast);
+	}
+
+	// Prueba con una ciudad que no existe y la fecha actual
+	@Test(expected = Exception.class)
+	public void testWeatherForecastNarnia() throws IOException {
+		String forecast = WeatherForecast.getCityWeather("Narnia", new Date());
+	}
+
+	// Prueba de tiempo óptimo
+	@Test(timeout = 100)
+	public void testWeatherForecastOptimo() throws IOException {
+		String forecast = WeatherForecast.getCityWeather("Madrid", new Date());
+		assertNotNull("Predicción no obtenida", forecast);
 	}
 
 }
